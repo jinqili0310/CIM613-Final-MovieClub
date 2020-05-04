9776752cbb50c010f0509643f39b55fc97971f4e
Jinqi Li <jxl2047@miami.edu>
2020-05-04 01:20:00 -0400

    part 1 code

--- MovieClub/MovieClub/LoginViewController.swift
+++ MovieClub/MovieClub/LoginViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+          return .lightContent
     }
 
 
---
+++ MovieClub/MovieClub/MovieView.swift
@@ -0,0 +1,14 @@
+//
+//  MovieView.swift
+//  MovieClub
+//
+//  Created by Jinqi Li on 5/3/20.
+//  Copyright © 2020 Jinqi Li. All rights reserved.
+//
+
+import UIKit
+
+class MovieView: UIView {
+    @IBOutlet weak var popMovie: UIImageView!
+    @IBOutlet weak var newMovie: UIImageView!
+}
